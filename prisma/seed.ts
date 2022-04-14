2534f0e6f07b1cb511114ec8f34a88e8ba3acfac
Mithat Ercan <mithat.ercan@hotmail.com>
2022-04-14 23:36:50 +0300

    Config the prisma and seed the data

---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+import prisma from '../lib/prisma';
+import bcrypt from 'bcrypt';
+import { artistsData } from './songsData';
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: 'user@test.com' },
+    update: {},
+    create: {
+      email: 'user@test.com',
+      password: bcrypt.hashSync('password', salt),
+      firstName: 'Mithat',
+      lastName: 'Ercan',
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      });
+    })
+  );
+};
+
+run()
+  .then()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
