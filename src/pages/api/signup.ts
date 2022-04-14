8367874bb6baf822a2e3fc62fa34c94ba714dff6
Mithat Ercan <mithat.ercan@hotmail.com>
2022-04-14 23:38:28 +0300

    Add signup api route

---
+++ src/pages/api/signup.ts
@@ -0,0 +1,50 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import prisma from '../../../lib/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+  const { email, password, firstName, lastName } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (e) {
+    res.status(401);
+    res.json({ error: 'User already exists' });
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    'hello',
+    { expiresIn: '8h' }
+  );
+
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('TRAX_ACCESS_TOKEN', token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: '/',
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+    })
+  );
+
+  res.json(user);
+};
