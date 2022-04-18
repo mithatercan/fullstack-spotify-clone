84f8b00c43431aa6e8650b3bb4d40af989cb6b9c
Mithat Ercan <mithat.ercan@hotmail.com>
2022-04-18 17:36:13 +0300

    Add auth components

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,66 @@
+import { Input, Button, Grid } from '@chakra-ui/react';
+import { useState } from 'react';
+import { auth } from '../../lib/mutations';
+import { useRouter } from 'next/router';
+const AuthForm = ({ mode }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('test');
+  const [lastName, setLastName] = useState('test');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const user = await auth(mode, { firstName, lastName, email, password });
+    if (user) {
+      setIsLoading(false);
+      router.push('/');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid
+        gap='20px'
+        padding='50px 30px'
+        bg='gray.900'
+        borderRadius='5px'
+        sx={{
+          input: {
+            width: '300px',
+            borderColor: 'gray.500',
+          },
+        }}
+      >
+        <Input
+          placeholder='Email'
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder='Password'
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button
+          _hover={{
+            bg: 'green.100',
+            color: 'green.500',
+          }}
+          width='100%'
+          type='submit'
+          bg='green.500'
+          isLoading={isLoading}
+        >
+          {mode}
+        </Button>
+      </Grid>
+    </form>
+  );
+};
+
+export default AuthForm;
---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,28 @@
+import { Box, Flex, Center, Input, Button, Grid } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+import { useSWRConfig } from 'swr';
+import { auth } from '../../lib/mutations';
+
+import Image from 'next/image';
+import AuthForm from './AuthForm';
+
+const AuthLayout: FC<{ mode: 'signin' | 'signup' }> = ({ mode }) => {
+  return (
+    <Box height='100vh' width='100vw' bg='black' color='white'>
+      <Flex
+        gap='30px'
+        direction='column'
+        justify='center'
+        align='center'
+        height='100vh'
+      >
+        <Center>
+          <Image src='/logo.png' height='90px' width='280px' />
+        </Center>
+        <AuthForm mode={mode} />
+      </Flex>
+    </Box>
+  );
+};
+
+export default AuthLayout;
